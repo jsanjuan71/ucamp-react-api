88313f027340ce6af07658ec702371d353bf3c6c
jsanjuandc <julio.sanjuan@dacodes.com.mx>
2023-08-18 20:06:44 -0600

    fixed  - add database host to health chek

--- src/services/healthCheck.ts
+++ src/services/healthCheck.ts
@@ -2,8 +2,8 @@ import { connect } from 'mongoose';
 
 const checkDatabase = async() : Promise<any> => {
     try {
-        const result = await connect(process.env.MDB_URL as string);
-        return true
+        const result = await connect(process.env.MDB_URL as string)
+        return result.connections[0].getClient().s.url
     } catch (error: any) {
         return error.message + " SRV= " + process.env.MDB_URL
     }
